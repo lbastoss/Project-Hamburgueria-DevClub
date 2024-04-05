5b478bb4749814b7701e62dd4600d0dd883fe752
lbastos17 <bastosls17@gmail.com>
2024-04-05 16:30:20 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,29 @@
+const list = document.querySelector('ul')
+let myLi = ''
+const buttonShowAll = document.querySelector('.show-all')
+
+
+function showAll() {
+
+    menuOptions.forEach((product) => {
+        myLi += `
+        <li>
+            <img src= ${product.src}>
+            <p> ${product.name}</p>
+            <p class="item-price"> R$ ${product.price}</p>
+        </li>
+
+    `
+
+    })
+
+    list.innerHTML = myLi
+    
+}
+
+
+buttonShowAll.addEventListener('click', showAll)
+
+
+
+
