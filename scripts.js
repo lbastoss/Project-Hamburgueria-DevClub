b2c8f298ed0a9a774c5c393b49b6c7742f0f05a5
lbastos17 <bastosls17@gmail.com>
2024-04-12 18:16:43 -0300

    adicionando função reduce

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
-
+const buttonSumAll = document.querySelector('.sum-all')
 
 function showAll(productsArray) {
     let myLi = ''
@@ -24,7 +24,7 @@ function showAll(productsArray) {
 
 
 function MapItens() {
-    
+
 
     const discount = menuOptions.map((products) => ({
 
@@ -33,12 +33,30 @@ function MapItens() {
 
     }))
 
-    showAll (discount)
+    showAll(discount)
 
 }
+
+
+
+function sumAllItens() {
+
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+    list.innerHTML = 
+    `
+    <li>
+    
+        <p> O total de todos os itens são R$ ${totalValue}</p>
+    
+    </li>
+    `
+}
+
+
 
 
 
 
 buttonShowAll.addEventListener('click', () => showAll(menuOptions));
 buttonMapAll.addEventListener('click', MapItens)
+buttonSumAll.addEventListener('click', sumAllItens)
